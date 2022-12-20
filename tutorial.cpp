2a72844dbfa3a3513f6640dbc495dc3d4be53d1a
Code My Hobby <65310505+codemyhobby100@users.noreply.github.com>
2022-12-20 11:22:45 +0000

    Add files via upload

---
+++ tutorial.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+ //COUT is an object that represents standard output stream
+ // << stream insertion operator
+// int main() {
+//     // int area_calc = 0;
+//     // int a = 1;
+//     // int b = 2;
+//     // int c = a;
+//     // a = b;
+//     // b = c;
+
+//     // std::cout << b;
+
+//     int a = 10;
+//     // int b = 3; 
+//     // int z = a % b;
+//     //int b = a++;  //Increasing x by 1(postfix) a will be 11 and b will be 10
+//     int c = ++a; // Increasing x b 1(prefix) both c and a will increase by 1 to give 11
+//     std:: cout << a;
+//     return 0;
+// }
+
+// int main() {
+//     double x = 10;
+//     double y = 5;
+//     double z = (x + 10) / (3 * y);
+//     std:: cout << z; 
+//     return 0;
+// }
+
+int main(){
+    double x = 10;
+    double y = 5;
+    double z = (x + 10) / (3 * y);
+    cout << "The final result = " << z  << endl 
+         << "Thank you for learning"; 
+    return 0;
+}
